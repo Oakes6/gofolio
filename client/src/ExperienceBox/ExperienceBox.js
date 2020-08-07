445277eb9ce3e20de4ed28e5298d5331a4b4331c
Tanner Oakes <tanner.w.oakes@gmail.com>
2020-08-06 21:04:15 -0500

    add modules and grid styling

---
+++ client/src/ExperienceBox/ExperienceBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ExperienceBox.css';
+function ExperienceBox() {
+
+    return (
+        <div className="grid">
+            <div>
+                <h1>Experience</h1>
+            </div>
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ExperienceBox;
--- client/src/NameBox/NameBox.js
+++ client/src/NameBox/NameBox.js
@@ -5,20 +5,11 @@ import CoffeeDoddle from '../CoffeeDoddle.svg'
 
 function NameBox() {
     return (
-        <div className="main">
-            <div>
-                <header></header>
-            </div>
-            <div>
-                <h1 className="primary">Tanner Oakes</h1>
-                <h1 className="secondary">Backend Developer based in TX</h1>
-                <img src={CoffeeDoddle} className="App-logo" />
-            </div>
-            <div className="experience">
-
-            </div>
-            <div className="projects">
-
+        <div className="grid">
+            <div className="namebox-grid">
+                <h1 className="primary-color">Tanner Oakes</h1>
+                <h1 className="secondary-color">Backend Developer based in TX</h1>
+                <img src={CoffeeDoddle} className="coffee-pic" alt="artsy doodle of coffee dude" />
             </div>
         </div>
     );
